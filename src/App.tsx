3e7458ca17400845c87c4a16bb648007e246550c
peachPetal <toneba3296@gmail.com>
2025-10-02 15:09:47 +0900

    🐛 DefaultLayout.tsx 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import DefaultLayout from './layouts/DefaultLayout';
 
-export default function App () {
+/**
+ * 애플리케이션의 메인 진입점 컴포넌트입니다.
+ * DefaultLayout을 통해 전체적인 구조를 잡고, 그 안에 페이지 콘텐츠를 넣습니다.
+ */
+export default function App() {
   return (
-    <div className="relative w-full mx-auto h-[1080px] bg-white">
-      <Header />
-      <Sidebar />
-    </div>
+    <DefaultLayout>
+    </DefaultLayout>
   );
 }
